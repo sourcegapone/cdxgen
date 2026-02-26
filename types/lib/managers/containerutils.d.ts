c775ec199a9690ba344fe10cd4354df063a1c0e8
prabhu <7842+prabhu@users.noreply.github.com>
2026-02-26 10:37:54 +0000

    Add qualifiers only for os packages (#3577)
    * Add qualifiers only for os packages
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    ---------
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ types/lib/managers/containerutils.d.ts
@@ -0,0 +1,3 @@
+export function getDirs(dirPath: string, dirName: string, hidden?: boolean, recurse?: boolean): string[];
+export function getOnlyDirs(srcpath: any, dirName: any): any[];
+//# sourceMappingURL=containerutils.d.ts.map
